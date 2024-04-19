4632d9c89729fd6e6bbb9b9fbc910e77ec2a12e0
FernandaFBMarques <mfernandafbmarques@gmail.com>
2024-04-19 18:39:26 -0300

    juncao codigo virna e cris em client.py

--- client.py
+++ client.py
@@ -1,23 +1,32 @@
 import socket
-import header
+from header import pack_header, calculate_checksum
+
 
 def create_client(host=socket.gethostname(), port=12345):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.connect((host, port))
         try:
+            seq_num = 1
             while True:
                 message = input("Digite a mensagem para enviar ou:\nDigite 1 para simular pacote perdido\nDigite 2 para simular o timeout no cliente\nEscreva: ")
                 if message.lower() == 'exit':
                     break
-                sock.sendall(message.encode())
-                response = sock.recv(1024)
+                payload = message.encode('utf-8')  # criptografa
+                checksum = calculate_checksum(payload)
+                # payload = sock.sendall(message.encode())
+                header = pack_header(seq_num, 0, 0b00000001, checksum, len(payload))
+                packet = header + payload
+                sock.sendall(packet)
+                response = sock.recv(1024)  # maximum amount of data to be received at once
+                seq_num += 1
                 if response == b'ACK':
-                    print("ACK received!")
+                    print(f"ACK received!, {seq_num}")
                 else:
                     print("No ACK, resending...")
         finally:
             print("Closing connection")
             sock.close()
+
 
 if __name__ == '__main__':
     create_client()
